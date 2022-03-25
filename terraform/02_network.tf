3a74145048b994272d3bed4ebfd69a50f1c5104c
mustafaerbay <mustafaerbay365@gmail.com>
2022-03-25 22:49:50 +0300

    subnet defined as a public with this route update

--- terraform/02_network.tf
+++ terraform/02_network.tf
@@ -100,3 +100,10 @@ resource "aws_internet_gateway" "production-igw" {
     Environment = "${var.environment}"
   }
 }
+
+# Route the public subnet traffic through the Internet Gateway
+resource "aws_route" "public-internet-igw-route" {
+  route_table_id         = aws_route_table.public-route-table.id
+  gateway_id             = aws_internet_gateway.production-igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
