75ac201c8c21a1d69842058eb53f5097a95df9c2
mustafaerbay <mustafaerbay365@gmail.com>
2022-03-30 01:37:13 +0300

    s3, iam, vpc endpoint connection added

---
+++ terraform/08_s3.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "my_static_website" {
+  bucket = "${var.bucketname}"
+
+  tags = {
+    Name        = "${var.tag}"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_s3_bucket_acl" "my_static_website" {
+  bucket = aws_s3_bucket.my_static_website.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "my_static_website" {
+  bucket = aws_s3_bucket.my_static_website.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "file" {
+  for_each = fileset(var.website_root, "**")
+
+  bucket      = aws_s3_bucket.my_static_website.id
+  key         = each.key
+  source      = "${var.website_root}/${each.key}"
+  source_hash = filemd5("${var.website_root}/${each.key}")
+  acl         = "public-read"
+}
